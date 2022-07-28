5a5e507ecd6b80c1602f0eac004cab4e213d9747
Kevin <macbookpro@MacBook-Pro-cua-Macbook.local>
2022-07-28 13:59:24 +0700

    Test-React

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import LogIn from "./screen/LogIn";
+import Register from "./screen/Register";
+import react from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
+  // return <LogIn />;
+  //return <Register/>
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LogIn">
+        <Stack.Screen name="LogIn" component={LogIn}/>
+        <Stack.Screen name="Register" component={Register}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screen/Register.js
@@ -0,0 +1,60 @@
+import react from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import UserInput from "../components/Input";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+const Register = ({navigation}) => {
+  return (
+    <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+      <Image source={require("../assets/logo.png")} style={styles.logo} />
+      <Text style={styles.title}>Register</Text>
+      <UserInput name="FullName" />
+      <UserInput name="Email" />
+      <UserInput name="Password" />
+      <UserInput name="Confirm Password" />
+      <TouchableOpacity style={styles.submit}>
+        <Text>Register</Text>
+      </TouchableOpacity>
+      <Text style={{ marginTop: 50 }}>
+        Do not have account? <Text onPress={() => navigation.navigate("LogIn")} style={{ color: "blue" }}>Login</Text>
+      </Text>
+    </KeyboardAwareScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 30,
+  },
+  logo: {
+    width: 270,
+    height: 150,
+    marginBottom: 10,
+    marginVertical: 60,
+  },
+  title: {
+    fontSize: 27,
+    color: "#2b3494",
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  submit: {
+    backgroundColor: "#FF9900",
+    height: 40,
+    justifyContent: "center",
+    borderRadius: 24,
+    marginHorizontal: 20,
+    alignItems: "center",
+    width: "100%",
+  },
+});
+export default Register;
